ace63c26a43bfcd3bee7f6633275b0a1d4f926a9
fabiodom84 <fabio@napfy.co>
2025-11-04 19:41:22 -0300

    redirect

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,30 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/tiktok',
+        destination: '/?utm_source=tiktok&utm_medium=bio&utm_campaign=organico',
+        permanent: false,
+      },
+      {
+        source: '/instagram',
+        destination: '/?utm_source=instagram&utm_medium=bio&utm_campaign=organico',
+        permanent: false,
+      },
+      {
+        source: '/facebook',
+        destination: '/?utm_source=facebook&utm_medium=bio&utm_campaign=organico',
+        permanent: false,
+      },
+      {
+        source: '/youtube',
+        destination: '/?utm_source=youtube&utm_medium=bio&utm_campaign=organico',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
