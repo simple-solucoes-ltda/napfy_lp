9a89cc43c29a0f5390c9975dc55cad0f606f7ff3
fabiodom84 <fabio@napfy.co>
2025-11-05 18:30:12 -0300

    fix: Correct template string syntax in noscript img src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
                 height="1"
                 width="1"
                 style={{ display: 'none' }}
-                src={\`https://www.facebook.com/tr?id=\${pixelId}&ev=PageView&noscript=1\`}
+                src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
               />
             </noscript>
           </>
