45aff620a7fa2c5fc0cabdbe526bc9525f145bb3
fabiodom84 <fabio@napfy.co>
2025-11-05 18:40:35 -0300

    fix: Use Script onLoad callback to prevent multiple Pixel initialization
    Moved fbq init to onLoad callback with proper guard to ensure single initialization across route changes.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import { type Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import clsx from 'clsx'
-import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/react'
 import { FirebaseAnalytics } from '@/components/FirebaseAnalytics'
+import { MetaPixel } from '@/components/MetaPixel'
 
 import '@/styles/tailwind.css'
 
@@ -27,49 +27,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID
-
   return (
     <html lang="pt-BR" className={clsx('bg-gray-50 antialiased', inter.variable)}>
-      <head>
-        {pixelId && (
-          <>
-            <Script
-              id="meta-pixel"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  !function(f,b,e,v,n,t,s)
-                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-                  'https://connect.facebook.net/en_US/fbevents.js');
-                  if (!window._fbq_initialized) {
-                    fbq('init', '${pixelId}');
-                    fbq('track', 'PageView');
-                    window._fbq_initialized = true;
-                  }
-                `,
-              }}
-            />
-            <noscript>
-              <img
-                height="1"
-                width="1"
-                style={{ display: 'none' }}
-                src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
-              />
-            </noscript>
-          </>
-        )}
-      </head>
       <body>
         {children}
         <Analytics />
         <FirebaseAnalytics />
+        <MetaPixel />
       </body>
     </html>
   )
